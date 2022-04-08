87d752513f6d21d15f8f451960a6c0b352cb8ffe
Ariel Ventura <arielventu@gmail.com>
2022-04-08 19:13:38 -0300

    April 8, 2022 7:13 PM

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,19 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const pokemonsRoutes = require('./pokemons.js');
-const typesRoutes = require('./types.js');
+
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use('/pokemons', pokemonsRoutes);
-router.use('/types', typesRoutes);
-
-router.get('/', (req, res) => {
-    res.send('estoy en el home');
-})
 
 module.exports = router;
