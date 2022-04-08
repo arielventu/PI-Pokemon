31735e7f6a34213a982a847748985a1ac33cca2e
Ariel Ventura <arielventu@gmail.com>
2022-04-08 19:38:04 -0300

    April 8, 2022 7:38 PM

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -19,12 +19,15 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => { 
     const { name, type, image } = req.body;
+    const exists = Pokemon.findOne({ where: { name } });
     const newPokemon = Pokemon.create({
         name,
         type,
         image
     })
-    res.send(newPokemon);
+    if (exists) { 
+        res.status(400).send('Pokemon ya creado');
+    } else res.send(newPokemon);
 })
 
 router.get('/', (req, res, next) => { 
