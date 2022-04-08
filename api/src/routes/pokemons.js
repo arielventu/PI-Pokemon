252207d99d38318353b716e657fde229f834ec15
Ariel Ventura <arielventu@gmail.com>
2022-04-08 19:21:27 -0300

    April 8, 2022 7:21 PM

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,9 +1,27 @@
 const { Router } = require("express");
-const { Pokemon } = require("../db.js");
+
 
 const router = Router()
 
+router.get('/', (req, res, next) => { 
+    res.send('Home Pokemons')
+})
 
+router.get('/:id', (req, res, next) => { 
+    res.send('Home Pokemons x ID')
+})
+
+router.get('/', (req, res, next) => { 
+    res.send('Home Pokemons')
+})
+
+router.get('/', (req, res, next) => { 
+    res.send('Home Pokemons')
+})
+
+router.get('/', (req, res, next) => { 
+    res.send('Home Pokemons')
+})
 
 
 module.exports = router;
