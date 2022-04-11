f689bf95c09effb4de53d799a4c245ec473df0c4
Ariel Ventura <arielventu@gmail.com>
2022-04-11 18:29:43 -0300

    April 11, 2022 6:29 PM

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -40,7 +40,7 @@ router.get('/:id', async (req, res, next) => {
     // const pokeDB = await Pokemon.findAll({ where: { id } })
     // console.log(`${URL_POKE}/${id}`);
     const pokeAPI = await axios.get(`${URL_POKE}/${id}`);
-    console.log(pokeAPI.data);
+    console.log(pokeAPI.data.name);
     try {
         res.send(pokeAPI.length ? pokeAPI : 'Pokemon no encontrado');
     } catch (error) {
