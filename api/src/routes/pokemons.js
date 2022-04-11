bae2f2d64f275f5b61b313692a1f63cec52bbfde
Ariel Ventura <arielventu@gmail.com>
2022-04-11 19:45:54 -0300

    April 11, 2022 7:45 PM

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -52,7 +52,7 @@ router.get('/:id', async (req, res, next) => {
         type: pokeAPI.data.types.map(type => type.type.name)
     }
     console.log(pokemonFound.id);
-    res.send('pokemonNotFound');
+    return res.status(400).send("Pokemon no encontrado");
     // try {
     //     // res.send(pokemonFound.data === 'Not Found' ? pokemonFound.data : pokemonFound);
     //     if (!pokemonFound.id) {
