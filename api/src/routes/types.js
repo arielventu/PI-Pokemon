3655661369c22ba67728a0e66511649bfa24605c
Ariel Ventura <arielventu@gmail.com>
2022-04-14 16:37:54 -0300

    April 14, 2022 4:37 PM

--- api/src/routes/types.js
+++ api/src/routes/types.js
@@ -3,8 +3,23 @@ const { Router } = require("express");
 
 const router = Router()
 
-router.get('/', (req, res, next) => { 
-    res.send('GET Home Types')
+router.get('/', async (req, res, next) => { 
+    const typeDB = await Type.findAll();
+    if (typeDB != null) {
+        res.status(200).send(typeDB);
+    } else {
+        const getAPI = await axios.get(`${URL_TYPE}`);
+        const typeAPIList = [];
+        for (let i = 0; i < getAPI.data.results.length; i++) {
+            const typeAPI = await axios.get(getAPI.data.results[i].url)
+            typeAPIList.push({
+                id: typeAPI.data.id,
+                name: capitalize(typeAPI.data.name)
+            });
+        }
+        const typeDB = await Type.bulkCreate(typeAPIList);
+        res.status(200).send(typeDB);
+    }
 })
 
 module.exports = router;
