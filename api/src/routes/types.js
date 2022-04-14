be50f68d039a95c39412498604f3056e8a218f84
Ariel Ventura <arielventu@gmail.com>
2022-04-14 16:43:08 -0300

    April 14, 2022 4:43 PM

--- api/src/routes/types.js
+++ api/src/routes/types.js
@@ -4,21 +4,27 @@ const { Router } = require("express");
 const router = Router()
 
 router.get('/', async (req, res, next) => { 
-    const typeDB = await Type.findAll();
-    if (typeDB != null) {
-        res.status(200).send(typeDB);
-    } else {
-        const getAPI = await axios.get(`${URL_TYPE}`);
-        const typeAPIList = [];
-        for (let i = 0; i < getAPI.data.results.length; i++) {
-            const typeAPI = await axios.get(getAPI.data.results[i].url)
-            typeAPIList.push({
-                id: typeAPI.data.id,
-                name: capitalize(typeAPI.data.name)
-            });
+    try {
+        const typeDB = await Type.findAll();
+        if (typeDB != null) {
+            console.log('Desde DB');
+            res.status(200).send(typeDB);
+        } else {
+            const getAPI = await axios.get(`${URL_TYPE}`);
+            const typeAPIList = [];
+            for (let i = 0; i < getAPI.data.results.length; i++) {
+                const typeAPI = await axios.get(getAPI.data.results[i].url)
+                typeAPIList.push({
+                    id: typeAPI.data.id,
+                    name: capitalize(typeAPI.data.name)
+                });
+            }
+            const typeDB = await Type.bulkCreate(typeAPIList);
+            console.log('Desde API');
+            res.status(200).send(typeDB);
         }
-        const typeDB = await Type.bulkCreate(typeAPIList);
-        res.status(200).send(typeDB);
+    } catch (error) {
+        next(error);        
     }
 })
 
