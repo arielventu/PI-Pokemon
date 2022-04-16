7bf69b0c631d564c8dfd5b10f9234f1b71ea22f4
Ariel Ventura <arielventu@gmail.com>
2022-04-16 17:59:17 -0300

    April 16, 2022 5:59 PM

--- client/src/index.js
+++ client/src/index.js
@@ -9,13 +9,11 @@ import { Provider } from 'react-redux';
 import store from "./store/index";
 
 ReactDOM.render(
-  <React.StrictMode>
-   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  </React.StrictMode>,
+  <Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
   document.getElementById('root')
 );
 
