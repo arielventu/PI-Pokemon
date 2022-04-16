941ac91199acb99efb72499bee5552a44bf6ebd9
Ariel Ventura <arielventu@gmail.com>
2022-04-16 11:37:43 -0300

    April 16, 2022 11:37 AM

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -61,7 +61,7 @@ router.get('/', async (req, res, next) => {
             include: Type
         })
                 
-        console.log(pokeDBList.toJSON());
+        console.log(pokeDBList[pokemon]);
 
         // const pokeDBList = pokeDB.map(pokemon => pokemon.dataValues);
         //Convertir a un array de objetos desde DB
