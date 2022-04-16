c75dfc98f362826c10a3120367217d75f25fee85
Ariel Ventura <arielventu@gmail.com>
2022-04-16 17:45:59 -0300

    April 16, 2022 5:45 PM

--- client/src/index.js
+++ client/src/index.js
@@ -6,15 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import store from "./store/index";
+import store from "./store/index";
 
 ReactDOM.render(
   <React.StrictMode>
-   {/* <Provider store={store}> */}
+   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  {/* </Provider>, */}
+  </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
