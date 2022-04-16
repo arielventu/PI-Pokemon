ef5aed8c71877908e22ab0a8727768804a65d196
Ariel Ventura <arielventu@gmail.com>
2022-04-16 13:19:10 -0300

    April 16, 2022 1:19 PM

--- api/src/utils.js
+++ api/src/utils.js
@@ -6,9 +6,14 @@ function capitalize (string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function startCapital(word){
+    return word.charAt(0) === word.charAt(0).toUpperCase()
+}
+
 module.exports = {
     URL_POKE,
     URL_TYPE,
     URL_SERVER,
     capitalize,
+    startCapital
 }
