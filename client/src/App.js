6d217bad3b726c8d4e83f35d21364a2ffb7baa26
Ariel Ventura <arielventu@gmail.com>
2022-04-18 19:27:29 -0300

    April 18, 2022 7:27 PM

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Route} from 'react-router-dom';
 import './App.css';
+import LandingPage from './components/LandingPage/';
+import NavBar from './components/NavBar/';
+import CardContainer from './components/CardContainer/';
+
 
 function App() {
   return (
     <React.Fragment>
-      <Route exact path="/home" component={NavBar} />
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/home" component={NavBar} />
       <Route exact path="/home" component={CardContainer} />
       <Route path="/home" component={Favorites} />
       <Route path="/movie/:id" component={Movie} />
