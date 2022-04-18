3eb5c1b9e4deb63ea07f7c39182e6ca0be995d24
Ariel Ventura <arielventu@gmail.com>
2022-04-18 19:55:31 -0300

    April 18, 2022 7:55 PM

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/LandingPage/index';
-// import NavBar from './components/NavBar/';
-// import CardContainer from './components/CardContainer/';
-// import SearchBar from './components/SearchBar/';
+import NavBar from './components/NavBar/';
+import CardContainer from './components/CardContainer/';
+import SearchBar from './components/SearchBar/';
 // import PokemonDetail from './components/PokemonDetail/';
 // import PokemonCreate from './components/PokemonCreate/';
 
@@ -14,10 +14,10 @@ function App() {
   return (
     <React.Fragment>
       <Route exact path="/" component={LandingPage} />
-      {/* <Route path="/home" component={NavBar} />
+      <Route path="/home" component={NavBar} />
       <Route path="/home" component={CardContainer} />
       <Route path="/home" component={SearchBar} />
-      <Route path="/pokemon/:id" component={PokemonDetail} />
+      {/* <Route path="/pokemon/:id" component={PokemonDetail} />
       <Route path="/pokemon/create" component={PokemonCreate} /> */}
     </React.Fragment>
   );
