93ae2bf24f91ff5dd52be9860a6f05069866f7b8
Ariel Ventura <arielventu@gmail.com>
2022-04-18 18:10:10 -0300

    April 18, 2022 6:10 PM

--- client/src/components/CardDetail/CardDetail.js
+++ client/src/components/CardDetail/CardDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import './PokemonDetail.css';
+import './CardDetail.css';
 import { getAllUserPosts, getAllUsers } from '../../actions';
 
 
