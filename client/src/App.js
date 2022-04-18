894545e50653e12d054cc7b66e3e93354e1940f7
Ariel Ventura <arielventu@gmail.com>
2022-04-18 19:28:25 -0300

    April 18, 2022 7:28 PM

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ function App() {
     <React.Fragment>
       <Route exact path="/" component={LandingPage} />
       <Route path="/home" component={NavBar} />
-      <Route exact path="/home" component={CardContainer} />
-      <Route path="/home" component={Favorites} />
+      <Route path="/home" component={CardContainer} />
+      <Route path="/pokemon/:id" component={Favorites} />
       <Route path="/movie/:id" component={Movie} />
       </React.Fragment>
   );
