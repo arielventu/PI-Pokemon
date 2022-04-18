5e1d31d439d775defc92b00630a79d3786187f7b
Ariel Ventura <arielventu@gmail.com>
2022-04-18 19:51:21 -0300

    April 18, 2022 7:51 PM

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Route} from 'react-router-dom';
 import './App.css';
-import LandingPage from './components/LandingPage/';
-import NavBar from './components/NavBar/';
-import CardContainer from './components/CardContainer/';
-import SearchBar from './components/SearchBar/';
-import PokemonDetail from './components/PokemonDetail/';
-import PokemonCreate from './components/PokemonCreate/';
+import LandingPage from './components/LandingPage/index';
+// import NavBar from './components/NavBar/';
+// import CardContainer from './components/CardContainer/';
+// import SearchBar from './components/SearchBar/';
+// import PokemonDetail from './components/PokemonDetail/';
+// import PokemonCreate from './components/PokemonCreate/';
 
 
 
@@ -14,11 +14,11 @@ function App() {
   return (
     <React.Fragment>
       <Route exact path="/" component={LandingPage} />
-      <Route path="/home" component={NavBar} />
+      {/* <Route path="/home" component={NavBar} />
       <Route path="/home" component={CardContainer} />
       <Route path="/home" component={SearchBar} />
       <Route path="/pokemon/:id" component={PokemonDetail} />
-      <Route path="/pokemon/create" component={PokemonCreate} />
+      <Route path="/pokemon/create" component={PokemonCreate} /> */}
       </React.Fragment>
   );
 }
