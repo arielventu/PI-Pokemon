bc6c93884d29eaeb9ddd3eb44d20495c4f6afebc
Ariel Ventura <arielventu@gmail.com>
2022-04-18 19:20:57 -0300

    April 18, 2022 7:20 PM

---
+++ client/src/components/LandingPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>Landing Page</h1>
+            <Link to="/home">
+                <button>Entrar</button>
+            </Link>
+        </div>
+  )
+}
+
+export default LandingPage
