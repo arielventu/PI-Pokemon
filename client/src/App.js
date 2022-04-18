1d3c87f22e7d1b02999afc7be65b7ec41c025ce2
Ariel Ventura <arielventu@gmail.com>
2022-04-18 18:28:18 -0300

    April 18, 2022 6:28 PM

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
-
+import NavBar from "./components/NavBar/NavBar";
+import SearchBar from "./components/SearchBar/SearchBar";
+import CardContainer from "./components/CardContainer/CardContainer";
 // import NavBar from "./components/NavBar/NavBar";
 // import Buscador from "./components/SearchBar/SearchBar";
 // import Users from "./components/CardContainer/CardContainer";
@@ -12,9 +14,10 @@ function App() {
   return (
       <React.Fragment>
         <Route exact path='/' component={LandingPage} />
-        {/* <Route path='/home' component={NavBar} />
-        <Route path='/home' component={SearchBar} />
-        <Route path='/users/:id/posts' component={UserPosts} />
+        <Route exact path='/home' component={NavBar} />
+        <Route exact path='/home' component={SearchBar} />
+        <Route exact path='/home' component={CardContainer} />
+        {/* <Route path='/users/:id/posts' component={UserPosts} />
         <Route path='/users/:id/posts' component={CommentsPost} />
         <Route path='/home' component={Buscador} /> */}
          
