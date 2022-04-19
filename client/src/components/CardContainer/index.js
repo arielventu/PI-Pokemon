b712ef6ed43b366047e68d3848b2ce42337638fd
Ariel Ventura <arielventu@gmail.com>
2022-04-19 18:19:38 -0300

    April 19, 2022 6:19 PM

--- client/src/components/CardContainer/index.js
+++ client/src/components/CardContainer/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import ALL_POKES from '../../utils';
 import { getPokemons } from '../../actions';
 import { useSelector, useDispatch } from 'react-redux';
 
