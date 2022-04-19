8af98e50230b093835b19c28d8bbfbe584d196a0
Ariel Ventura <arielventu@gmail.com>
2022-04-19 18:18:32 -0300

    April 19, 2022 6:18 PM

--- client/src/components/CardContainer/index.js
+++ client/src/components/CardContainer/index.js
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import ALL_POKES from '../../utils';
 import { getPokemons } from '../../actions';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 
 const CardContainer = () => {
-    
-        
+    const [pokemons, setPokemons] = useState([]);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPokemons());
+    }, [dispatch]);
+
+    const pokemonsFromStore = useSelector(state => state.pokemons);
+
+    useEffect(() => {
+        setPokemons(pokemonsFromStore);
+    }, [pokemonsFromStore]);
+  
 
   return (
       <div>
           <h1>CardContainer</h1>
           <div className="card-container">
-              {pokemon.map(pokemon => (
+              {pokemons.map(pokemon => (
                   <div className="card" key={pokemon.id}>
-                      <h2>{pokemon.name}</h2>
                       <img src={pokemon.image} alt={pokemon.name} />
+                      <h3>{pokemon.name}</h3>
+                      <p>{pokemon.types.map(type => type.type.name).join(', ')}</p>
                   </div>
-              ))}
-              
+                ))}
           </div>
-      </div>
-  )
+     </div>
+    );
 };
 
 
