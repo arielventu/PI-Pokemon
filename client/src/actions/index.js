71a6c34f0f9115bbf7f6cdc8b807fa5d2cbeac56
Ariel Ventura <arielventu@gmail.com>
2022-04-19 18:23:59 -0300

    April 19, 2022 6:23 PM

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,7 +5,9 @@ export const GET_POKEMONS = 'GET_POKEMONS';
 
 export const getPokemons = () => async (dispatch) => {
     try {
+        console.log(ALL_POKES);
         const res = await axios.get(ALL_POKES);
+        // console.log(res.data);
         dispatch({ type: 'GET_POKEMONS', payload: res.data });
     } catch (err) {
         console.log(err);
