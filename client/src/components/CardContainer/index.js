91c2da637c7e19de5ec8527b3c50b51de56ce341
Ariel Ventura <arielventu@gmail.com>
2022-04-19 23:16:49 -0300

    April 19, 2022 11:16 PM

--- client/src/components/CardContainer/index.js
+++ client/src/components/CardContainer/index.js
@@ -7,7 +7,24 @@ import { useSelector, useDispatch } from 'react-redux';
 const CardContainer = () => {
     const dispatch = useDispatch();
     const pokemons = useSelector(state => state.pokemons);
-  
+    const [selectedPokemon, setSelectedPokemon] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        dispatch(getPokemons());
+    }, [dispatch]);
+
+    const handleSelectPokemon = (pokemon) => {
+        setSelectedPokemon(pokemon);
+    }
+
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    const filteredPokemons = pokemons.filter(pokemon => {
+        return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
 
     return (
         <div>
