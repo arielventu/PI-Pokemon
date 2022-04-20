3779a735ce4e27354b6dd0da6b03acadf5c3a8c5
Ariel Ventura <arielventu@gmail.com>
2022-04-20 14:10:49 -0300

    April 20, 2022 2:10 PM

--- client/src/components/CardContainer/index.js
+++ client/src/components/CardContainer/index.js
@@ -15,7 +15,7 @@ const CardContainer = () => {
     if (pokemons.length === 0) {
         return <div>
             Loading...
-            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/04de2e31234507.564a1d23645bf.gif" alt="loading" />
+            <img src="https://i.pinimg.com/originals/4e/a2/3e/4ea23e6339937b95a8aa5cd08eeb3266.gif" alt="loading" />
         </div>      
     } 
         return (
