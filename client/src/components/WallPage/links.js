8e8c858b44a5a9f3bce21e7674be2d903dc89492
Ariel Ventura <arielventu@gmail.com>
2022-04-20 16:46:18 -0300

    April 20, 2022 4:46 PM

--- client/src/components/WallPage/links.js
+++ client/src/components/WallPage/links.js
@@ -1,1 +1,5 @@
-export const URL1 = ""
+export const URL1 = "https://img.wallpapersafari.com/desktop/1920/1080/92/59/kaidGe.jpg"
+export const URL2 = ""
+
+
+// export const URL1 = "./img/kaidGe.jpg"
