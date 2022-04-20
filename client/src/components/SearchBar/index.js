a7de0c2dac27dda5a89aed819613842513c4ece6
Ariel Ventura <arielventu@gmail.com>
2022-04-20 20:00:17 -0300

    April 20, 2022 8:00 PM

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -8,11 +8,15 @@ const SearchBar = () => {
     e.preventDefault()
     console.log(search)
   }
+
+  const onChange = (e) => {
+    setSearch(e.target.value)
+  }
   
   return (
     <div>
       <form onSubmit={onSubmit} >
-        <input type="text" />
+        <input type="text"  />
         <input type="submit" value="Search" />
       </form>
     </div>
