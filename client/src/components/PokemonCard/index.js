86560aa2c0b4b7fea67a73131888d6867eb542c7
Ariel Ventura <arielventu@gmail.com>
2022-04-21 09:12:29 -0300

    April 21, 2022 9:12 AM

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -6,11 +6,11 @@ const PokemonCard = (name, image, type, id) => {
   return (
     <div>
       <h1>PokemonCard</h1>
-         <div key={id}>
+         
               <h1>Name: {name}</h1>
               <img src={image} alt={name} />
-              <h3>Type: { type[1] ? type[0] + ' ' + type[1] : type }</h3>
-          </div>
+              {/* <h3>Type: { type[1] ? type[0] + ' ' + type[1] : type }</h3> */}
+          
     </div>
   )
 }
