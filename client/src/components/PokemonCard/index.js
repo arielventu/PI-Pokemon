ef489189b7826c04f59ccd6fbb6d7dde7d6787a1
Ariel Ventura <arielventu@gmail.com>
2022-04-21 11:31:02 -0300

    April 21, 2022 11:31 AM

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -6,7 +6,7 @@ const PokemonCard = (name, image, type, id) => {
   return (
     <div>
 			<div className='div_poke_card'>
-				<img className='img' src={image} width="200" height="200" alt='Image' />
+				<img className='img' src={image} width="200" height="200" alt='' />
 				<div className='div_name'>{name}</div>
 				<div className='div_type'>{type}</div>
 			</div>
