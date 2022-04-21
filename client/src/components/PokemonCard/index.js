c9d742d3e834e116676f2e44a2cec755f1531ca1
Ariel Ventura <arielventu@gmail.com>
2022-04-21 08:51:30 -0300

    April 21, 2022 8:51 AM

---
+++ client/src/components/PokemonCard/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { getPokemons } from '../../actions';
+import { useSelector, useDispatch } from 'react-redux';
+
+const PokemonCard = () => {
+  return (
+    <div>PokemonCard</div>
+  )
+}
+
+export default PokemonCard
