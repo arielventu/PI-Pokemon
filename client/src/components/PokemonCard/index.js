74a04c7215b8511d49869a58a749c2303d9f3f39
Ariel Ventura <arielventu@gmail.com>
2022-04-22 09:52:56 -0300

    April 22, 2022 9:52 AM

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -14,7 +14,7 @@ export default function PokemonCard ({name, id, image, type}) {
 							type.map((el, i) => (
 								<div key={i} className='{cssButtonType(el)}'>
 									<p key={i} className='p'>
-										{el.name}
+										{el}
 									</p>
 								</div>
 							))}
