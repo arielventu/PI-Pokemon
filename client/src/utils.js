328d1c465827389c95ebb9127da921308069a0b2
Ariel Ventura <arielventu@gmail.com>
2022-04-22 15:33:10 -0300

    April 22, 2022 3:33 PM

--- client/src/utils.js
+++ client/src/utils.js
@@ -2,3 +2,4 @@ export const ALL_POKES = 'http://localhost:3001/pokemons/';
 export const LOADING_IMG = "https://raw.githubusercontent.com/Codelessly/FlutterLoadingGIFs/master/packages/cupertino_activity_indicator_selective.gif"
 
 
+
