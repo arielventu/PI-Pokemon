ee8665002c19f9b6a629e3a55f453cb4b8e8d5c9
Ariel Ventura <arielventu@gmail.com>
2022-04-22 10:51:39 -0300

    April 22, 2022 10:51 AM

--- client/src/components/CardContainer/index.js
+++ client/src/components/CardContainer/index.js
@@ -7,16 +7,26 @@ import './CardContainer.css';
 
 
 const CardContainer = () => {
+
+    // **************************************************************************************
+    // **************************************STATE*******************************************
+
     const dispatch = useDispatch();
     const pokemons = useSelector(state => state.pokemons);
   
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pokemonsPerPage] = useState(12);
+    const [loading, setLoading] = useState(true);
+
+    // **************************************************************************************
+    // **************************************FUNCTIONS****************************************
+    
     useEffect(() => {
         if (pokemons.length === 0) { // si no están cargados en el store, se cargan
             dispatch(getPokemons());
         }
     }, []);
-
-       
+    
     // console.log(pokemons);
 
     if (pokemons.length === 0) {
