3702878418fb3cd95c99d06606876c4ee8e4b6d2
Ariel Ventura <arielventu@gmail.com>
2022-04-22 18:22:42 -0300

    April 22, 2022 6:22 PM

--- client/src/components/PokemonDetail/index.js
+++ client/src/components/PokemonDetail/index.js
@@ -2,10 +2,35 @@ import React from 'react'
 import { getPokemons } from '../../actions'
 
 const PokemonDetail = () => {
+  const dispatch = useDispatch()
+  const pokemons = useSelector(state => state.pokemons)
+
+  useEffect(() => {
+    if (pokemons.length === 0) {
+      dispatch(getPokemons())
+    }
+  }, [])
+
+
+  
   return (
-      <div>
-          <h1>Pokemon Detail</h1>
-          
+    <div>
+      <h1>Pokemon Detail</h1>
+        <div>
+          {pokemons.map(pokemon => (
+            <div key={pokemon.id}>
+              <h2>{pokemon.name}</h2>
+              <img src={pokemon.image} alt={pokemon.name} width="105" height="105" />
+              <div>
+                {pokemon.type && pokemon.type.map((type, i) => (
+                  <div key={i} className={`type ${type}`}>
+                    <p key={i}>{type}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))} 
+        </div>           
     </div>
   )
 }
