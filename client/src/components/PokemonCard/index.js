0c980aec7c9861fe77f408dae62244026f4554fd
Ariel Ventura <arielventu@gmail.com>
2022-04-24 14:43:20 -0300

    April 24, 2022 2:43 PM

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './PokemonCard.css';
 
-
-
 const PokemonCard = ({name, id, image, type, attack}) => {
   return (
 		<div className='pokemon-card'>
 			<h1>Name:{name}</h1>
 			<img src={image} alt={name} width="105" height="105" />
-			<h2>Attack: {attack}</h2>
+		  <h2>Attack: {attack}</h2>
+		  console.log({attack})
 		  <div className='div-type'>
 				{type &&
 					type.map((el, i) => (
