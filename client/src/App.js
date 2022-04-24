88f6c6fde065208ff9e63884c1314d78bdc80449
Ariel Ventura <arielventu@gmail.com>
2022-04-24 11:08:39 -0300

    April 24, 2022 11:08 AM

--- client/src/App.js
+++ client/src/App.js
@@ -18,8 +18,8 @@ function App() {
       {/* <Route path="/" component={Loading} /> */}
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/home" component={NavBar} />
-      <Route exact path="/home" component={Filters} />
       <Route exact path="/home" component={SearchBar} />
+      <Route exact path="/home" component={Filters} />
       <Route exact path="/home" component={Home} />
       {/* <Route path="/pokemon/:id" component={PokemonDetail} /> */}
       {/* <Route path="/pokemon/create" component={PokemonCreate} /> */}
