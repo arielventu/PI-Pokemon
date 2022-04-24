121bd8633d167f8ffe64d58e6629f064200cf6da
Ariel Ventura <arielventu@gmail.com>
2022-04-24 14:41:54 -0300

    April 24, 2022 2:41 PM

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -8,8 +8,8 @@ const PokemonCard = ({name, id, image, type, attack}) => {
 		<div className='pokemon-card'>
 			<h1>Name:{name}</h1>
 			<img src={image} alt={name} width="105" height="105" />
+			<h2>Attack: {attack}</h2>
 		  <div className='div-type'>
-			  <h2>Attack: {attack}</h2>
 				{type &&
 					type.map((el, i) => (
 						<div key={i} className='{cssButtonType(el)}'>
