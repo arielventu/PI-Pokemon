1eb5f8be6b9d1d9b52c310fb649ebe0c5a93eb47
Ariel Ventura <arielventu@gmail.com>
2022-04-24 09:44:45 -0300

    April 24, 2022 9:44 AM

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -86,14 +86,6 @@ const CardContainer = () => {
         setOrderBy(orderBy);
     }
 
-    // (function sortPokemons (p) { // Función para ordenar la lista de pokemons
-    //     if (order === 'Ascendente') {
-    //         return p.sort((a, b) => (a[orderBy] > b[orderBy]) ? 1 : -1);
-    //     } else {
-    //         return p.sort((a, b) => (a[orderBy] < b[orderBy]) ? 1 : -1);
-    //     }
-    // })(pokemonsToShow); // Se autoejecuta la función y se pasa como parámetro la lista de pokemons
-
     // Render ********************************************************************************
 
     if (pokemons.length === 0) {
