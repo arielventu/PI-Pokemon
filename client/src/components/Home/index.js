da1e9eaf1df482086835df162618d69cc01ff49b
Ariel Ventura <arielventu@gmail.com>
2022-04-24 10:39:55 -0300

    April 24, 2022 10:39 AM

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -126,6 +126,12 @@ const CardContainer = () => {
                         <button onClick={() => sortBy(orderBy)}>{order}</button>
                     </div>
                 </div>
+                <div calssName='home-header-filter'>
+                    <select onChange={(e) => filterBy(e.target.value)}>
+                        <option value='Todos'>Todos</option>
+                        {types.map(type => <option key={type.id} value={type.name}>{type.name}</option>)}
+                    </select>
+                </div>
                 <div className='home-body'>
                     {pokemonsToShow.map((p) => (
                         // console.log(p.type)
