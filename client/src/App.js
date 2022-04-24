cb9739102709462069bf4e92a8e73c2debfff9a8
Ariel Ventura <arielventu@gmail.com>
2022-04-24 11:03:46 -0300

    April 24, 2022 11:03 AM

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 // import Loading from './components/Loading';
 import LandingPage from './components/LandingPage/';
 import NavBar from './components/NavBar/';
-import Home from './components/Home';
 import Filters from './components/Filters'
+import Home from './components/Home';
 import SearchBar from './components/SearchBar/';
 // import PokemonDetail from './components/PokemonDetail/';
 // import PokemonCreate from './components/PokemonCreate/';
@@ -18,8 +18,8 @@ function App() {
       {/* <Route path="/" component={Loading} /> */}
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/home" component={NavBar} />
-      <Route exact path="/home" component={SearchBar} />
       <Route exact path="/home" component={Filters} />
+      <Route exact path="/home" component={SearchBar} />
       <Route exact path="/home" component={Home} />
       {/* <Route path="/pokemon/:id" component={PokemonDetail} /> */}
       {/* <Route path="/pokemon/create" component={PokemonCreate} /> */}
