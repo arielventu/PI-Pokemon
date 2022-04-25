cc694a59c928e1731ce94415fe0aeaba5a757348
Ariel Ventura <arielventu@gmail.com>
2022-04-25 16:04:55 -0300

    April 25, 2022 4:04 PM

--- client/src/components/Filters/index.js
+++ client/src/components/Filters/index.js
@@ -20,27 +20,42 @@ const Filters = () => {
 
    
   return (
-    <div>
-      <div className='home-filters'>
-        <div className='home-filters-types'>
-            <select onChange={(e) => handleChange(e.target.value)}>
-            {/* <select> */}
-                <option value='All'>All</option>
-                {types.map(type => (
-                    <option key={type.id} value={type.name}>{type.name}</option>
-                ))}
-            </select>
-        </div>
-        <div className='home-filters-origin'>
-            <select>
-            {/* <select onChange={(e) => filterBy(e.target.value)}> */}
-                <option value='All'>All</option>
-                <option value='PokeAPI'>PokeAPI</option>
-                <option value='Created'>Created</option>
-            </select>
-        </div>
+    <div className="filters">
+      <h3>Filters</h3>
+      <div className="filters__types">
+        <h4>Types</h4>
+        <select onChange={handleChange}>
+          <option value="">All</option>
+          {types.map((type) => (
+            <option key={type.id} value={type.name}>
+              {type.name}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
-    </div>
+    
+    // <div>
+    //   <div className='home-filters'>
+    //     <div className='home-filters-types'>
+    //         <select onChange={(e) => handleChange(e.target.value)}>
+    //         {/* <select> */}
+    //             <option value='All'>All</option>
+    //             {types.map(type => (
+    //                 <option key={type.id} value={type.name}>{type.name}</option>
+    //             ))}
+    //         </select>
+    //     </div>
+    //     <div className='home-filters-origin'>
+    //         <select>
+    //         {/* <select onChange={(e) => filterBy(e.target.value)}> */}
+    //             <option value='All'>All</option>
+    //             <option value='PokeAPI'>PokeAPI</option>
+    //             <option value='Created'>Created</option>
+    //         </select>
+    //     </div>
+    // </div>
+    // </div>
   )
 }
 
