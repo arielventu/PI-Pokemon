44454911df1f87e5ba7da110f411a8c889469c05
Ariel Ventura <arielventu@gmail.com>
2022-04-26 18:06:08 -0300

    April 26, 2022 6:06 PM

--- client/src/components/Pagination/index.js
+++ client/src/components/Pagination/index.js
@@ -7,17 +7,16 @@ const Pagination = ({allPokemons, pokemonsPerPage, pagination}) => {
         pageNumbers.push(i);
     }
 
-    const renderPageNumbers = pageNumbers.map(number => {
-        return (
+            return (
             <nav key={number}>
                 <h1>PAGINACIONNN</h1>
-                <li className="page-numbers">
+                
+                    <li className="page-numbers">
                     <button onClick={() => pagination(number)}>{number}</button>
                 </li>
             </nav>
         );
 
-    });
-}
+    }
 
 export default Pagination
