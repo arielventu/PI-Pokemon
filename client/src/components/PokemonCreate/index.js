d57d3fd495d7fab6e9d3fb0c05d29dd4ed660b84
Ariel Ventura <arielventu@gmail.com>
2022-04-27 18:10:39 -0300

    April 27, 2022 6:10 PM

--- client/src/components/PokemonCreate/index.js
+++ client/src/components/PokemonCreate/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux'
-import { getTypes } from '../../actions'
+import { getTypes, createPokemon } from '../../actions'
 
 const validate = ({ name, hp, attack, defense, speed, height, weight }) => {
     const errors = {};
