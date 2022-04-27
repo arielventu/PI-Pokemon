f6148540c783fa976d791a191b5955b065d07a34
Ariel Ventura <arielventu@gmail.com>
2022-04-27 17:42:34 -0300

    April 27, 2022 5:42 PM

---
+++ client/src/components/PokemonCreate/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import { getTypes } from '../../actions'
+
+
+
+export const index = () => {
+  return (
+    <div>index</div>
+  )
+}
