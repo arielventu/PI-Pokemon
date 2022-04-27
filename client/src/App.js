c32f18f1c54a0ca7a0dcf3ce53872eaaa94b7ef4
Ariel Ventura <arielventu@gmail.com>
2022-04-27 18:07:09 -0300

    April 27, 2022 6:07 PM

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 import LandingPage from './components/LandingPage/';
 import Home from './components/Home';
 // import NavBar from './components/NavBar/';
-// import Filters from './components/Filters/';
 // import SearchBar from './components/SearchBar/';
 // import PokemonDetail from './components/PokemonDetail/';
-// import PokemonCreate from './components/PokemonCreate/';
+import PokemonCreate from './components/PokemonCreate/';
 
 
 
@@ -18,9 +17,8 @@ function App() {
       <Route exact path="/home" component={Home} />
       {/* <Route exact path="/home" component={NavBar} />
       <Route exact path="/home" component={SearchBar} />
-      <Route exact path="/home" component={Filters} /> */}
       {/* <Route path="/pokemon/:id" component={PokemonDetail} /> */}
-      {/* <Route path="/pokemon/create" component={PokemonCreate} /> */}
+      <Route path="/pokemon/create" component={PokemonCreate} />
     </React.Fragment>
   );
 }
--- client/src/components/PokemonCreate/index.js
+++ client/src/components/PokemonCreate/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux'
 import { getTypes } from '../../actions'
@@ -17,12 +17,151 @@ const validate = ({ name, hp, attack, defense, speed, height, weight }) => {
     return errors;
 };
 
-export const PokemonCreate = () => {
+export default function PokemonCreate () {
+    const dispatch = useDispatch();
+    const types = useSelector(state => state.types);
+    const [pokemon, setPokemon] = useState({
+        name: '',
+        hp: '',
+        attack: '',
+        defense: '',
+        speed: '',
+        height: '',
+        weight: '',
+        type: []
+    });
+    const [errors, setErrors] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleChange = (e) => {
+        setPokemon({
+            ...pokemon,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSelect = (e) => {
+        setPokemon({
+            ...pokemon,
+            type: [...e.target.options].filter(o => o.selected).map(o => o.value)
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const errors = validate(pokemon);
+        setErrors(errors);
+        setIsSubmitting(true);
+        if (Object.keys(errors).length === 0) {
+            dispatch(createPokemon(pokemon));
+            setPokemon({
+                name: '',
+                hp: '',
+                attack: '',
+                defense: '',
+                speed: '',
+                height: '',
+                weight: '',
+                type: []
+            });
+        }
+    };
+
+    useEffect(() => {
+        dispatch(getTypes());
+    }, [dispatch]);
+
+
   return (
       <div>
           <h1>Create Pokemon</h1>
-          
-          
-    </div>
+          <form onSubmit={handleSubmit}>
+              <div>
+                  <label htmlFor="name">Name</label>
+                  <input
+                      type="text"
+                      name="name"
+                      value={pokemon.name}
+                      onChange={handleChange}
+                  />
+                  {errors.name && <p>{errors.name}</p>}   
+              </div>
+              <div>
+                  <label htmlFor="hp">HP</label>
+                  <input
+                      type="text" 
+                      name="hp"
+                      value={pokemon.hp}
+                      onChange={handleChange}
+                  />  
+                  {errors.hp && <p>{errors.hp}</p>}   
+              </div>
+              <div>
+                  <label htmlFor="attack">Attack</label>
+                  <input
+                      type="text"
+                      name="attack"
+                      value={pokemon.attack}
+                      onChange={handleChange}
+                  />  
+                  {errors.attack && <p>{errors.attack}</p>}   
+              </div>
+              <div>
+                  <label htmlFor="defense">Defense</label>    
+                  <input
+                      type="text"
+                      name="defense"
+                      value={pokemon.defense}
+                      onChange={handleChange}
+                  />
+                  {errors.defense && <p>{errors.defense}</p>} 
+              </div>
+              <div>
+                  <label htmlFor="speed">Speed</label>
+                  <input
+                      type="text"
+                      name="speed"
+                      value={pokemon.speed}
+                      onChange={handleChange}
+                  />
+                  {errors.speed && <p>{errors.speed}</p>} 
+              </div>  
+              <div>
+                  <label htmlFor="height">Height</label>  
+                  <input
+                      type="text"
+                      name="height"
+                      value={pokemon.height}
+                      onChange={handleChange}
+                  />
+                  {errors.height && <p>{errors.height}</p>}   
+              </div>
+              <div>
+                  <label htmlFor="weight">Weight</label>  
+                  <input
+                      type="text"
+                      name="weight"
+                      value={pokemon.weight}
+                      onChange={handleChange}
+                  />
+                  {errors.weight && <p>{errors.weight}</p>}   
+              </div>
+              <div>
+                  <label htmlFor="type">Type</label>
+                  <select
+                      name="type"
+                      multiple
+                      value={pokemon.type}
+                      onChange={handleSelect}
+                  >
+                      {types.map(type => (
+                          <option key={type.id} value={type.name}>{type.name}</option>
+                      ))}
+                  </select>
+              </div>
+              <button type="submit">Submit</button>
+          </form>
+          <Link to="/">Back to Home</Link>
+      </div>
   )
 }
