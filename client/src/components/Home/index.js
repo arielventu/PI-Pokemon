bffa1ebbb1a1f54c8045aa0d4227961dce99ba2e
Ariel Ventura <arielventu@gmail.com>
2022-04-27 09:54:50 -0300

    April 27, 2022 9:54 AM

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getPokemons } from '../../actions';
+import { getPokemons, filterByOrigin } from '../../actions';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PokemonCard from '../PokemonCard';
@@ -58,7 +58,12 @@ const Home = () => {
             dispatch(getPokemons());
     }, [dispatch]);
 
+    const handleFilterByOrigin = (origin) => {
+        dispatch(filterByOrigin(origin));
+    };
+
     
+        
 
     // Render ********************************************************************************
 
