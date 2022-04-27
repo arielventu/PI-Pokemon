e084175a0673e11cd3b0919936ba246ab8ddba91
Ariel Ventura <arielventu@gmail.com>
2022-04-27 19:29:55 -0300

    April 27, 2022 7:29 PM

--- client/src/components/PokemonCreate/index.js
+++ client/src/components/PokemonCreate/index.js
@@ -63,6 +63,7 @@ export default function PokemonCreate () {
                 speed: '',
                 height: '',
                 weight: '',
+                image: '',
                 type: []
             });
         }
