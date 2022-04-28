724c53254709c26476a47f9765922bb78e043888
Ariel Ventura <arielventu@gmail.com>
2022-04-28 19:24:07 -0300

    April 28, 2022 7:24 PM

--- client/src/components/PokemonDetail/index.js
+++ client/src/components/PokemonDetail/index.js
@@ -4,7 +4,7 @@ import { getPokemons, getPokemonDetails } from '../../actions';
 import { Link } from 'react-router-dom';
 
 
-export default function PokemonDetail (name, hp, attack, defense, speed, height, weight, image, type) {
+export default function PokemonDetail (props) {
   // console.log(props);
   
   const dispatch = useDispatch()
