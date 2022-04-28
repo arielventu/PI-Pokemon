cdad2c66b70bd7b79bd1bfe5cc03ab72bfbd90c9
Ariel Ventura <arielventu@gmail.com>
2022-04-28 17:05:07 -0300

    April 28, 2022 5:05 PM

--- client/src/components/PokemonCreate/index.js
+++ client/src/components/PokemonCreate/index.js
@@ -180,8 +180,10 @@ export default function PokemonCreate () {
                       onChange={handleChange}
                   />
               </div>
-              { Object.keys(errors).length !== 0 }
-              <button type="submit">Submit</button>
+              {Object.keys(errors).length !== 0 ?
+                  <button type="submit" disabled>Create</button> :
+                  <button type="submit">Create</button>}
+              {/* <button type="submit">Submit</button> */}
           </form>
           <Link to="/home">Back to Home</Link>
       </div>
