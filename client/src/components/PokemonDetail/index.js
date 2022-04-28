8cd5a7c225784154741b3e52cc653f5431085e5d
Ariel Ventura <arielventu@gmail.com>
2022-04-28 19:08:35 -0300

    April 28, 2022 7:08 PM

--- client/src/components/PokemonDetail/index.js
+++ client/src/components/PokemonDetail/index.js
@@ -4,38 +4,28 @@ import { getPokemons, getPokemonDetails } from '../../actions';
 import { Link } from 'react-router-dom';
 
 
-export default function PokemonDetail () {
+export default function PokemonDetail (props) {
+  console.log(props);
+  
   const dispatch = useDispatch()
   const pokemons = useSelector(state => state.pokemons)
-  const pokemonDetails = useSelector(state => state.detailPokemon)
-  const pokemonFound = pokemons.find(pokemon => pokemon.id === pokemonDetails.id)
+  const pokemonDetail = useSelector(state => state.detailPokemon)
+  const pokemonFound = pokemons.find(pokemon => pokemon.id === pokemonDetail.id)
 
   useEffect(() => {
-    if (pokemons.length === 0) {
-      dispatch(getPokemons())
-    }
-  }, [dispatch, pokemons])
-
+    dispatch(getPokemonDetails())
+  }, [dispatch])
 
   
   return (
     <div>
-      {/* <h1>Pokemon Detail</h1>
+      {pokemonDetail.length > 0 && 
         <div>
-          {pokemons.map(pokemon => (
-            <div key={pokemon.id}>
-              <h2>{pokemon.name}</h2>
-              <img src={pokemon.image} alt={pokemon.name} width="105" height="105" />
-              <div>
-                {pokemon.type && pokemon.type.map((type, i) => (
-                  <div key={i} className={`type ${type}`}>
-                    <p key={i}>{type}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))} 
-        </div>            */}
+          <h1>{pokemonDetail.name}</h1>
+        </div>
+        }
+    
+      
     </div>
   )
 }
