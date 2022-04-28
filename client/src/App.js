5e658be392f668d1ae908e2438dd56459b5723fe
Ariel Ventura <arielventu@gmail.com>
2022-04-28 20:32:38 -0300

    April 28, 2022 8:32 PM

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,8 @@ function App() {
   return (
     <React.Fragment>
       <Route exact path="/" component={LandingPage} />
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/home" component={SearchBar} />
+      <Route path="/home" component={Home} />
+      <Route path="/home" component={SearchBar} />
       <Route exact path="/pokemon/:id" component={PokemonDetail} />
       {/* <Route exact path="/home" component={NavBar} /> */}
       <Route path="/pokemon/create" component={PokemonCreate} />
