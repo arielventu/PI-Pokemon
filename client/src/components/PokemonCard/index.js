052f1ac1bdd956720363bad7f67ef5ef3cb158aa
Ariel Ventura <arielventu@gmail.com>
2022-04-30 09:53:17 -0300

    April 30, 2022 9:53 AM

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import style from './PokemonCard.module.css';
 
 export default function PokemonCard ({name, id, image, type, attack}) {
+
 	
-	const typeColor = black => {
-		
-		if (black) {
-			return '#000';
-		} else {
-			return '#fff';
+	const typeColor = (e) => {
+	if (type[0].name === 'fire') {
+		return '#ff0000'
 		}
-	}
+		if (type[0].name === 'water') {
+		return '#0000ff'
+		}
+		if (type[0].name === 'grass') {
+		return '#00ff00'
+		}
+}
 
 return (
 	<div className={`${style.divCard}`}>
@@ -18,7 +22,7 @@ return (
 		<img src={image} alt={name} width="105" height="105" />
 		<div className='div-type'>
 			{type && type.map((el, i) => (
-				<div key={i} className={`${style.typeColor}`}>
+				<div key={i} className={`${style[typeColor]}`}>
 					<p key={i} className='p'>
 						{el}
 					</p>
