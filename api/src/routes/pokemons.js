15fa045f3e3c336f807c81dd57921b05c7df8879
Ariel Ventura <arielventu@gmail.com>
2022-04-29 22:04:53 -0300

    April 29, 2022 10:04 PM

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -101,7 +101,7 @@ router.get('/', async (req, res, next) => {
             });
         }
         
-        const pokeAPI = await axios.get(`${URL_POKE}/?offset=0&limit=40`); 
+        const pokeAPI = await axios.get(`${URL_POKE}/?offset=0&limit=8`); 
         const pokeAPIList = []; 
             
         //Convertir a un array de objetos desde API
