30388cc9f6d810f7a0a82fb47977924fa8031e54
Ariel Ventura <arielventu@gmail.com>
2022-04-30 09:54:11 -0300

    April 30, 2022 9:54 AM

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -4,17 +4,7 @@ import style from './PokemonCard.module.css';
 export default function PokemonCard ({name, id, image, type, attack}) {
 
 	
-	const typeColor = (e) => {
-	if (type[0].name === 'fire') {
-		return '#ff0000'
-		}
-		if (type[0].name === 'water') {
-		return '#0000ff'
-		}
-		if (type[0].name === 'grass') {
-		return '#00ff00'
-		}
-}
+	
 
 return (
 	<div className={`${style.divCard}`}>
