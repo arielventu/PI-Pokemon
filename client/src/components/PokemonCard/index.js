4a380b3908298df132f7c6fcb008126a604850dc
Ariel Ventura <arielventu@gmail.com>
2022-04-30 10:10:13 -0300

    April 30, 2022 10:10 AM

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -12,7 +12,7 @@ return (
 		<img src={image} alt={name} width="105" height="105" />
 		<div className={`${style.types}`}>
 			{type && type.map((el, i) => (
-				<div key={i} className={`${style[{el}]}`}>
+				<div key={i} className={`${style[el]}`}>
 					<p key={i} className='p'>
 						{el}
 					</p>
