1c980fe1c65e8193a54c5573a01ff3a3b95db673
Ariel Ventura <arielventu@gmail.com>
2022-04-30 11:45:11 -0300

    April 30, 2022 11:45 AM

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -10,10 +10,10 @@ return (
 	<div className={`${style.divCard}`}>
 		<h1 className={`${style.name}`}>{name}</h1>
 		<img src={image} alt={name} width="105" height="105" />
-		<div >
+		<div className={`${style.divTypes}`} >
 			{type && type.map((type, index) => (
 				// <div key={index} className={`${style.divType}`}>
-				<div key={index} className={`${style[type[0]]}`}>
+				<div key={index} className={`${style[type]}`}>
 					{/* <p key={index} className={`${style[type]}`} > */}
 					<p key={index}  >
 						{type}
