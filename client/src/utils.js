e923240f79ff15571160d1ef21e997858708eced
Ariel Ventura <arielventu@gmail.com>
2022-05-01 12:09:23 -0300

    May 1, 2022 12:09 PM

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,7 @@
 export const ALL_POKES = 'http://localhost:3001/pokemons/';
 export const POKES_NAME = 'http://localhost:3001/pokemons?name=';
 export const ALL_TYPES = 'http://localhost:3001/types/';
-export const LOADING_IMG = "https://66.media.tumblr.com/9697ebbc4887dc57620c50a12f24c61d/tumblr_nc1rokF7r31s1rd1xo1_500.gif"
+export const LOADING_IMG = "https://c.tenor.com/Hg2Mb_mQdhYAAAAi/pokemon-pokeball.gif"
 
 
 
