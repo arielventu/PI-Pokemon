14050168b067f4da8a7abff9bf89555e5bc6ec73
Ariel Ventura <arielventu@gmail.com>
2022-05-01 11:55:02 -0300

    May 1, 2022 11:55 AM

--- client/src/components/Pagination/index.js
+++ client/src/components/Pagination/index.js
@@ -3,13 +3,11 @@ import style from './Pagination.module.css'
 
 export default function Pagination ({allPokemons, pokemonsPerPage, pagination, currentPage}) {
     const pageNumbers = [];
+    console.log(currentPage);
 
     for (let i = 1; i <= Math.ceil(allPokemons/pokemonsPerPage); i++) {
         pageNumbers.push(i);
     }
-
-    console.log(currentPage);
-    console.log(pokemonsPerPage);
 
     return (
     <nav className={`${style.nav}`}>
