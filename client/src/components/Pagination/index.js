4fe65c10c8408dda7cbb59600151675fa9eeff2b
Ariel Ventura <arielventu@gmail.com>
2022-05-01 11:43:39 -0300

    May 1, 2022 11:43 AM

--- client/src/components/Pagination/index.js
+++ client/src/components/Pagination/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from './Pagination.module.css' 
 
-export default function Pagination ({allPokemons, pokemonsPerPage, pagination}) {
+export default function Pagination ({allPokemons, pokemonsPerPage, pagination, currentPage}) {
     const pageNumbers = [];
 
     for (let i = 1; i <= Math.ceil(allPokemons/pokemonsPerPage); i++) {
