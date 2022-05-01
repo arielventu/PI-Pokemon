dfe86d4d429a32f13e18eb8d11a44376a0dfc628
Ariel Ventura <arielventu@gmail.com>
2022-05-01 12:14:50 -0300

    May 1, 2022 12:14 PM

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,7 @@
 export const ALL_POKES = 'http://localhost:3001/pokemons/';
 export const POKES_NAME = 'http://localhost:3001/pokemons?name=';
 export const ALL_TYPES = 'http://localhost:3001/types/';
-export const LOADING_IMG = "https://i.pinimg.com/originals/8b/22/c2/8b22c2b446f9f539693c305d7e599ef6.gif"
+export const LOADING_IMG = "https://static.wikia.nocookie.net/mipequeoponyfanlabor/images/b/bf/Tumblr_mempzlLC3u1rmikpao1_400.gif/revision/latest?cb=20141201182914&path-prefix=es"
 
 
 
