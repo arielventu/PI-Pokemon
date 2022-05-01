db7356e7bd736ad1faf0158c7fe91a58db99187e
Ariel Ventura <arielventu@gmail.com>
2022-05-01 14:30:47 -0300

    May 1, 2022 2:30 PM

---
+++ client/src/components/NotFound/pokeNotFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import style from './NotFound.module.css'
+
+const NotFound = () => {
+    return (
+        <div className={`${style.div}`}>
+            <h1 className={`${style.h1}`}>POKEMON</h1>
+            <h2 className={`${style.h2}`}>Page not found</h2>
+            <Link to="/home" style={{ textDecoration: 'none' }}>
+                <button className={`${style.button}`}>Go to Home</button>
+            </Link>
+        </div>
+    )
+}
+
+export default NotFound
