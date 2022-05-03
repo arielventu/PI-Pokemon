69dac20d8e918400d89cd9b775a39482de9eebdf
Ariel Ventura <arielventu@gmail.com>
2022-05-03 09:43:17 -0300

    May 3, 2022 9:43 AM

--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -3,22 +3,72 @@ const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
 const { Pokemon, conn } = require('../../src/db.js');
+const modelPokemon = require('../../src/models/Pokemon.js');
+const modelType = require('../../src/models/Type.js');
 
 const agent = session(app);
 const pokemon = {
-  name: 'Pikachu',
+  name: 'PikachuTest',
+  id: 'a416d989-91d1-48c9-b583-267df138834c',
+
 };
 
 describe('Pokemon routes', () => {
   before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
   beforeEach(() => Pokemon.sync({ force: true })
     .then(() => Pokemon.create(pokemon)));
   describe('GET /pokemons', () => {
     it('should get 200', () =>
-      agent.get('/pokemons').expect(200)
+      agent.get('/pokemons/').expect(200)
     );
   });
-});
+
+  describe('GET /pokemons/:id', () => {
+    it('should get 200', function () {
+      agent.get(`/pokemons/${pokemon.id}`)
+        .expect(200)
+    });
+    it('should get Name', function () {
+      agent.get(`/pokemons/${pokemon.id}`)
+        .expect(200)
+        .expect(res => {
+          expect(res.body.name).to.equal(pokemon.name);
+        });
+    });
+  });
+
+  describe('GET /pokemons?name=', () => {
+    it('should get 200', function () {
+      agent.get(`/pokemons?name=${pokemon.name}`)
+        .expect(200)
+    });
+    it('should get Name', function () {
+      agent.get(`/pokemons?name=${pokemon.name}`)
+        .expect(200)
+        .expect(res => {
+          expect(res.body[0].name).to.equal(pokemon.name);
+        });
+    });
+  });
+  
+  
+  
+  describe('POST /pokemons', () => {
+    it('should get 200', function () {
+      agent.post('/pokemons/')
+        .send(pokemon)
+        .expect(200)
+    });
+    it('should get Name', function () {
+      agent.post('/pokemons/')
+        .send(pokemon)
+        .expect(200)
+        .expect(res => {
+          expect(res.body.name).to.equal(pokemon.name);
+        });
+    });
+  });
+  });
