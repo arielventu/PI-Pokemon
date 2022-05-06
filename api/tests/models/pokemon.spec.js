edbd181a75d3d8c85af81dc470fbf76cf0200b25
Ariel Ventura <arielventu@gmail.com>
2022-05-06 20:53:34 -0300

    May 6, 2022 8:53 PM

--- api/tests/models/pokemon.spec.js
+++ api/tests/models/pokemon.spec.js
@@ -54,6 +54,51 @@ describe('Pokemon model', () => {
         .then(() => done())
         .catch(() => done(new Error("Hp is not a number")));
     });
+  
+    it("Should throw an error if attack is not a number", (done) => {
+      Pokemon.create({ name: "pikachu2", attack: "aaa" })
+        .then(() => done())
+        .catch(() => done(new Error("Attack is not a number")));
+    });
+
+    it("This test should run fine, strength is a number", (done) => {
+      Pokemon.create({ name: "pikachu2", attack: 15 })
+        .then(() => done())
+        .catch(() => done(new Error("Attack is not a number")));
+    });
+  
+    it("Should throw an error if defense is not a number", (done) => {
+      Pokemon.create({ name: "pikachu3", defense: "aaa" })
+        .then(() => done())
+        .catch(() => done(new Error("Defense is not a number")));
+    });
+  
+    it("Should throw an error if speed is not a number", (done) => {
+      Pokemon.create({ name: "pikachu4", speed: "aaa" })
+        .then(() => done())
+        .catch(() => done(new Error("Speed is not a number")));
+    });
+  
+    it("Should throw an error if height is not a number", (done) => {
+      Pokemon.create({ name: "pikachu5", height: "aaa" })
+        .then(() => done())
+        .catch(() => done(new Error("Height is not a number")));
+    });
+  
+    it("Should throw an error if weight is not a number", (done) => {
+      Pokemon.create({ name: "pikachu6", weight: "aaa" })
+        .then(() => done())
+        .catch(() => done(new Error("Weight is not a number")));
+    });
+  
+    it("It works if some values aren't passed", () => {
+      Pokemon.create({ name: "pikachu7" });
+      Pokemon.create({ name: "pikachu8", hp: 50});
+      Pokemon.create({ name: "pikachu9", defense: 50 });
+      Pokemon.create({ name: "pikachu10", speed: 50 });
+      Pokemon.create({ name: "Pikachu11", height: 50 });
+    });
+  
 
     it('Should throw an error if name is repeated', (done) => {
       Pokemon.create({ name: 'pika2' });
