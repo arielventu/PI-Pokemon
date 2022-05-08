73e572045ff4dd60c8e957f83e6a4edefadf4db3
Ariel Ventura <arielventu@gmail.com>
2022-05-08 13:24:13 -0300

    May 8, 2022 1:24 PM

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -36,7 +36,7 @@ export default function Home () {
     };
 
     const handleFilterByType = (e) => {
-        msjTypes.length ? alert(msjTypes) : dispatch(filterByType(e.target.value));
+        dispatch(filterByType(e.target.value));
     };
 
     const handleSort = (e) => {
@@ -68,7 +68,7 @@ export default function Home () {
                         <select className={`${style.selectFilterType}`} defaultValue={"default"} onChange={(e) => handleFilterByType(e)}>
                             <option value={"default"} hidden>Type</option>
                             <option value='All'>All</option>
-                            {allTypes.map((type) => (
+                            {msjTypes.length ? alert(msjTypes) : allTypes.map((type) => (
                                 <option key={type.id} value={type.name}>
                                     {type.name}
                                 </option>
