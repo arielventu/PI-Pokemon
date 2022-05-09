aa3d82ad2b4beb7b3c0f287e297f97fd3d3a3f26
Ariel Ventura <arielventu@gmail.com>
2022-05-09 13:37:44 -0300

    May 9, 2022 1:37 PM

--- client/src/App.js
+++ client/src/App.js
@@ -14,19 +14,16 @@ import NotFound from './components/NotFound'
 function App() {
   return (
     <Router>
+      <Route exact path="/" component={LandingPage} />
       <NavBar />
-      {/* <Switch> */}
-        <Route exact path="/" component={LandingPage} />
+      <Switch>
         {/* <Route exact path="/home" component={NavBar} /> */}
         <Route exact path="/home" component={Home} />
         <Route exact path="/pokemon/:id" component={PokemonDetail} />
         <Route exact path="/pokemon/search/:name" component={PokemonDetailByName} />
         <Route exact path="/create" component={PokemonCreate} />
-        <Route path="/">
-            <NotFound />
-          </Route>
-        {/* <Route path="/" component={NotFound} /> */}
-      {/* </Switch> */}
+        <Route path="/" component={NotFound} />
+      </Switch>
     </Router>
   );
 }
