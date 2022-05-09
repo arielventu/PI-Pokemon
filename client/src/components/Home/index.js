abc57a85a8b306a1380ca02913a7e95bdc6a273e
Ariel Ventura <arielventu@gmail.com>
2022-05-09 14:16:18 -0300

    May 9, 2022 2:16 PM

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -26,8 +26,8 @@ export default function Home () {
     };
 
     useEffect(() => {
-        dispatch(getPokemons());
         dispatch(getTypes());
+        dispatch(getPokemons());
         dispatch(clearPokemonDetails());
     }, []);
     
