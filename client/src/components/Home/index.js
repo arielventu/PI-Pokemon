a660423553222fb007e14f0c8f5db2ac760bdd2e
Ariel Ventura <arielventu@gmail.com>
2022-05-09 19:32:44 -0300

    May 9, 2022 7:32 PM

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -15,7 +15,7 @@ export default function Home () {
   
     const [currentPage, setCurrentPage] = useState(1);
     const [pokemonsPerPage, setPokemonsPerPage] = useState(12);
-    const lastPokeOfThePage = currentPage * pokemonsPerPage;
+    const lastPokeOfThePage = currentPage * pokemonsPerPage; // 12
     const firstPokeOfThePage = lastPokeOfThePage - pokemonsPerPage;
     const currentPokemons = allPokemons.slice(firstPokeOfThePage, lastPokeOfThePage);
 
