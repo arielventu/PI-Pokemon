c909e0d7d120f3aa3b7f80dd3da0088f798df27b
Ariel Ventura <arielventu@gmail.com>
2022-05-11 13:25:34 -0300

    May 11, 2022 1:25 PM

--- client/src/components/PokemonCreate/index.js
+++ client/src/components/PokemonCreate/index.js
@@ -184,6 +184,7 @@ export default function PokemonCreate() {
                         <select
                             className={`${style.selectType}`}
                             name="type"
+                            
                             multiple
                             value={pokemon.type}
                             onChange={handleSelect}
