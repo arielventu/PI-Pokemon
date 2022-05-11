0ffa5d647ef34ee095b7bcbc6d557c7238ec1492
Ariel Ventura <arielventu@gmail.com>
2022-05-11 14:09:56 -0300

    May 11, 2022 2:09 PM

--- client/src/components/PokemonCreate/index.js
+++ client/src/components/PokemonCreate/index.js
@@ -54,9 +54,10 @@ export default function PokemonCreate() {
     const handleSelect = (e) => {
         setPokemon({
             ...pokemon,
-                type: [...e.target.options].filter(o => o.selected).map(o => o.value)
+            // type: [...e.target.options].filter(o => o.selected).map(o => o.value)
+                type: setIsSelected([...isSelected, e.target.value])
         })
-        setIsSelected([...isSelected, e.target.value])
+        isSelected > 2 ? setIsSelected([...isSelected, e.target.value]) : alert('Only up to two types can be selected')
         // console.log('e.target.value ', e.target.value);
         // console.log('isSelected ', isSelected)
         // console.log('isSelected.length ', isSelected.length)
