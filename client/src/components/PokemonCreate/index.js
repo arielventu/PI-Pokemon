63a99c44ce1a9b8d78971059028257f9d7e7fc6b
Ariel Ventura <arielventu@gmail.com>
2022-05-11 16:30:07 -0300

    May 11, 2022 4:30 PM

--- client/src/components/PokemonCreate/index.js
+++ client/src/components/PokemonCreate/index.js
@@ -69,7 +69,7 @@ export default function PokemonCreate() {
     const removeSelected = (e) => {
         setPokemon({
             ...pokemon,
-            type: [...isSelected, setIsSelected(isSelected.filter(type => type !== e.target.value))]
+            type: [...isSelected, isSelected.find(e.target.value)]
         })
         setIsSelected(isSelected.filter(type => type !== e.target.value))
     }
