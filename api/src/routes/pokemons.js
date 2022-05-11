04a3857d77c6c5c6120dc674086cbd0b889c3060
Ariel Ventura <arielventu@gmail.com>
2022-05-11 11:13:24 -0300

    May 11, 2022 11:13 AM

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -216,7 +216,8 @@ router.post('/', async (req, res, next) => {
                 }
                 // console.log(newPokemon[type] = type)
                 const newPokeWithType = newPokemon.type = type
-                return res.json(newPokeWithType).status(201);
+                // return res.json(newPokeWithType).status(201);
+                return res.json(newPokemon).status(201)
             } else {
                 return res.json("Name already exists in the database");
             }
